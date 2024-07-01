7e5543a0e3c87f9035a2949111d0c6056d672a8d
Nicherke <nicksoncheruiyot001@gmail.com>
2024-07-01 21:12:13 +0300

    first commit

---
+++ src/loop.java
@@ -0,0 +1,10 @@
+public class loop {
+    public static void main(String[] args) {
+        double hello = 2;
+        double var1 = 4;
+
+        for (double i = var1; i<20; i+=hello) {
+            System.out.println(i);
+        }
+    }
+}
